d6ef791011602ea176abd39b524a1da1f596e7ee
Jeff Garzik <jeff@garzik.org>
2009-03-22 20:08:15 -0400

    libcldc: delete unused and incomplete struct cld_client handling
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- include/cldc.h
+++ include/cldc.h
@@ -100,23 +100,6 @@ struct cldc {
 	GHashTable	*sessions;
 };
 
-struct cld_client {
-	int			fd;		/* UDP socket */
-
-	char			host[256];
-	struct sockaddr_in6	addr;
-	socklen_t		addrlen;
-
-	struct sockaddr_in6	local_addr;
-	socklen_t		local_addrlen;
-	int			local_port;
-};
-
-extern int cldcli_init(void);
-extern void cldcli_free(struct cld_client *);
-extern struct cld_client *cldcli_new(const char *remote_host, int remote_port,
-				   int local_port);
-
 extern int cldc_new_sess(struct cldc *cldc, const struct cldc_call_opts *copts,
 			 const void *addr, size_t addr_len,
 			 struct cldc_session **sess_out);
