2be57b3a8ecebc6d94b7b34c9b9a885ddc1227e1
Jeff Garzik <jeff@garzik.org>
2009-07-31 21:58:31 -0400

    include/chunk_msg.h: better struct chunksrv_req documentation
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- include/chunk_msg.h
+++ include/chunk_msg.h
@@ -35,15 +35,15 @@ enum errcode {
 };
 
 struct chunksrv_req {
-	uint8_t			magic[CHD_MAGIC_SZ];
-	uint8_t			op;
-	uint8_t			resp_code;
+	uint8_t			magic[CHD_MAGIC_SZ];	/* CHUNKD_MAGIC */
+	uint8_t			op;			/* CHO_xxx */
+	uint8_t			resp_code;		/* errcode's */
 	uint8_t			rsv1[2];
-	uint32_t		nonce;
-	uint64_t		data_len;
-	char			user[CHD_USER_SZ];
-	char			key[CHD_KEY_SZ];
-	char			checksum[CHD_CSUM_SZ];
+	uint32_t		nonce;	/* random number, to stir checksum */
+	uint64_t		data_len;		/* len of addn'l data */
+	char			user[CHD_USER_SZ];	/* username */
+	char			key[CHD_KEY_SZ];	/* object id */
+	char			checksum[CHD_CSUM_SZ];	/* SHA1 checksum */
 };
 
 struct chunksrv_resp_get {
