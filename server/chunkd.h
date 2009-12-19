26f1b76fc53b110b2525f616ccb6dd40e0eda716
Jeff Garzik <jeff@garzik.org>
2009-12-19 17:38:31 -0500

    Canonicalize copyright headers.
    Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

--- server/chunkd.h
+++ server/chunkd.h
@@ -1,5 +1,23 @@
-#ifndef __STORAGED_H__
-#define __STORAGED_H__
+#ifndef __CHUNKD_H__
+#define __CHUNKD_H__
+
+/*
+ * Copyright 2009 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
 
 #include <stdbool.h>
 #include <netinet/in.h>
@@ -297,4 +315,4 @@ extern size_t strnlen(const char *s, size_t maxlen);
 extern int daemon(int nochdir, int noclose);
 #endif
 
-#endif /* __STORAGED_H__ */
+#endif /* __CHUNKD_H__ */
