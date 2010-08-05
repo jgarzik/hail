jgarzik/hail

commit ea5d20bc22aeed077312c9c1824e84651af17a16

[chunk] protocol, disk fmt: Replace ASCII checksum representation with binary
Rather than converting SHA1 checksums back and forth between ASCII
and binary, always store and compare binary checksums.  Only convert
to ASCII when performing a list-objects request, which requires
XML output.

Among other savings, this decreases the size of the per-message
fixed-length header by 44 bytes.

Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

include/chunk_msg.h
@@ -27,7 +27,7 @@ enum {
 	CHD_MAGIC_SZ		= 8,
 	CHD_USER_SZ		= 64,
 	CHD_KEY_SZ		= 1024,	/* key size limit; max 65534 (fffe) */
-	CHD_CSUM_SZ		= 64,
+	CHD_CSUM_SZ		= 20,	/* == SHA_DIGEST_LENGTH */
 	CHD_SIG_SZ		= 64,
 };
 
@@ -90,7 +90,7 @@ struct chunksrv_resp {
 	uint8_t			rsv1[3];
 	uint32_t		nonce;	/* txn id, copied from request */
 	uint64_t		data_len;		/* len of addn'l data */
-	char			checksum[CHD_CSUM_SZ];	/* SHA1 checksum */
+	unsigned char		hash[CHD_CSUM_SZ];	/* SHA1 checksum */
 };
 
 struct chunksrv_resp_get {

chunkd/be-fs.c
@@ -62,7 +62,7 @@ struct be_fs_obj_hdr {
 
 	char			reserved[16];
 
-	char			checksum[128];
+	unsigned char		hash[CHD_CSUM_SZ];
 	char			owner[128];
 } __attribute__ ((packed));
 
@@ -506,8 +506,7 @@ struct backend_obj *fs_obj_open(uint32_t table_id, const char *user,
 		goto err_out_read;
 	}
 
-	strncpy(obj->bo.hashstr, hdr.checksum, sizeof(obj->bo.hashstr));
-	obj->bo.hashstr[sizeof(obj->bo.hashstr) - 1] = 0;
+	memcpy(obj->bo.hash, hdr.hash, sizeof(obj->bo.hash));
 	obj->bo.size = value_len;
 	obj->bo.mtime = st.st_mtime;
 
@@ -638,7 +637,7 @@ ssize_t fs_obj_sendfile(struct backend_obj *bo, int out_fd, size_t len)
 #endif /* HAVE_SENDFILE && HAVE_SYS_SENDFILE_H */
 
 bool fs_obj_write_commit(struct backend_obj *bo, const char *user,
-				const char *hashstr, bool sync_data)
+			 unsigned char *md, bool sync_data)
 {
 	struct fs_obj *obj = bo->private;
 	struct be_fs_obj_hdr hdr;
@@ -646,7 +645,7 @@ bool fs_obj_write_commit(struct backend_obj *bo, const char *user,
 
 	memset(&hdr, 0, sizeof(hdr));
 	memcpy(hdr.magic, BE_FS_OBJ_MAGIC, strlen(BE_FS_OBJ_MAGIC));
-	strncpy(hdr.checksum, hashstr, sizeof(hdr.checksum));
+	memcpy(hdr.hash, md, sizeof(hdr.hash));
 	strncpy(hdr.owner, user, sizeof(hdr.owner));
 	hdr.key_len = GUINT32_TO_LE(bo->key_len);
 	hdr.value_len = GUINT64_TO_LE(obj->written_bytes);
@@ -873,7 +872,7 @@ void fs_list_objs_close(struct fs_obj_lister *t)
  * Read an object by filename.
  * TODO - possibly factor out some code from fs_obj_open and fs_obj_delete.
  */
-int fs_obj_hdr_read(const char *fn, char **owner, char **csum,
+int fs_obj_hdr_read(const char *fn, char **owner, unsigned char *hash,
 		    void **keyp, size_t *klenp,
 		    unsigned long long *size, time_t *mtime)
 {
@@ -945,11 +944,8 @@ int fs_obj_hdr_read(const char *fn, char **owner, char **csum,
 		applog(LOG_WARNING, "NO CORE");
 		goto err_owner;
 	}
-	*csum = strndup(hdr.checksum, sizeof(hdr.checksum));
-	if (!*csum) {
-		applog(LOG_WARNING, "NO CORE");
-		goto err_csum;
-	}
+
+	memcpy(hash, hdr.hash, sizeof(hdr.hash));
 
 	*keyp = key_in;
 	*klenp = klen_in;
@@ -959,8 +955,6 @@ int fs_obj_hdr_read(const char *fn, char **owner, char **csum,
 	close(fd);
 	return 0;
 
- err_csum:
-	free(*owner);
  err_owner:
  err_var:
 	free(key_in);
@@ -988,16 +982,17 @@ GList *fs_list_objs(uint32_t table_id, const char *user)
 
 	while (fs_list_objs_next(&lister, &fn) > 0) {
 		char *owner;
-		char *csum;
 		unsigned long long size;
 		time_t mtime;
 		struct volume_entry *ve;
 		void *p;
+		unsigned char md[CHD_CSUM_SZ];
 		size_t alloc_len;
 		void *key_in;
 		size_t klen_in;
+		char hashstr[(CHD_CSUM_SZ * 2) + 1];
 
-		rc = fs_obj_hdr_read(fn, &owner, &csum, &key_in, &klen_in,
+		rc = fs_obj_hdr_read(fn, &owner, md, &key_in, &klen_in,
 				     &size, &mtime);
 		if (rc < 0) {
 			free(fn);
@@ -1005,23 +1000,23 @@ GList *fs_list_objs(uint32_t table_id, const char *user)
 		}
 		free(fn);
 
+		hexstr(md, CHD_CSUM_SZ, hashstr);
+
 		/* filter out results that do not match
 		 * the authenticated user
 		 */
 		if (strcmp(user, owner)) {
 			free(owner);
-			free(csum);
 			free(key_in);
 			continue;
 		}
 
 		/* one alloc, for fixed + var length struct */
-		alloc_len = sizeof(*ve) + strlen(csum) + 1 + strlen(owner) + 1;
+		alloc_len = sizeof(*ve) + strlen(hashstr)+1 + strlen(owner) + 1;
 
 		ve = malloc(alloc_len);
 		if (!ve) {
 			free(owner);
-			free(csum);
 			free(key_in);
 			applog(LOG_ERR, "OOM");
 			break;
@@ -1040,7 +1035,7 @@ GList *fs_list_objs(uint32_t table_id, const char *user)
 
 		p = (ve + 1);
 		ve->hash = p;
-		strcpy(ve->hash, csum);
+		strcpy(ve->hash, hashstr);
 
 		p += strlen(ve->hash) + 1;
 		ve->owner = p;
@@ -1050,14 +1045,13 @@ GList *fs_list_objs(uint32_t table_id, const char *user)
 		res = g_list_append(res, ve);
 
 		free(owner);
-		free(csum);
 	}
 
 	fs_list_objs_close(&lister);
 	return res;
 }
 
-int fs_obj_do_sum(const char *fn, unsigned int klen, char **csump)
+int fs_obj_do_sum(const char *fn, unsigned int klen, unsigned char *md)
 {
 	enum { BUFLEN = 128 * 1024 };
 	void *buf;
@@ -1065,8 +1059,6 @@ int fs_obj_do_sum(const char *fn, unsigned int klen, char **csump)
 	ssize_t rrc;
 	int rc;
 	SHA_CTX hash;
-	char *csum;
-	unsigned char md[SHA_DIGEST_LENGTH];
 
 	rc = ENOMEM;
 	buf = malloc(BUFLEN);
@@ -1097,19 +1089,10 @@ int fs_obj_do_sum(const char *fn, unsigned int klen, char **csump)
 	}
 	SHA1_Final(md, &hash);
 
-	csum = malloc(SHA_DIGEST_LENGTH*2 + 1);
-	if (!csum) {
-		rc = ENOMEM;
-		goto err_retdup;
-	}
-	hexstr(md, SHA_DIGEST_LENGTH, csum);
-
 	close(fd);
 	free(buf);
-	*csump = csum;
 	return 0;
 
- err_retdup:
  err_read:
 	close(fd);
  err_seek:

chunkd/chunkd.h
@@ -136,7 +136,7 @@ struct backend_obj {
 
 	uint64_t		size;
 	time_t			mtime;
-	char			hashstr[50];
+	unsigned char		hash[CHD_CSUM_SZ];
 };
 
 enum st_cld {
@@ -270,7 +270,7 @@ extern ssize_t fs_obj_write(struct backend_obj *bo, const void *ptr, size_t len)
 extern ssize_t fs_obj_read(struct backend_obj *bo, void *ptr, size_t len);
 extern void fs_obj_free(struct backend_obj *bo);
 extern bool fs_obj_write_commit(struct backend_obj *bo, const char *user,
-				const char *hashstr, bool sync_data);
+				unsigned char *md, bool sync_data);
 extern bool fs_obj_delete(uint32_t table_id, const char *user,
 		          const void *kbuf, size_t klen,
 			  enum chunk_errcode *err_code);
@@ -280,14 +280,15 @@ extern int fs_list_objs_open(struct fs_obj_lister *t,
 			     const char *root_path, uint32_t table_id);
 extern int fs_list_objs_next(struct fs_obj_lister *t, char **fnp);
 extern void fs_list_objs_close(struct fs_obj_lister *t);
-extern int fs_obj_hdr_read(const char *fn, char **owner, char **checksum,
+extern int fs_obj_hdr_read(const char *fn, char **owner,
+			   unsigned char *hash,
 			   void **keyp, size_t *klenp,
 			   unsigned long long *size, time_t *mtime);
 extern GList *fs_list_objs(uint32_t table_id, const char *user);
 extern bool fs_table_open(const char *user, const void *kbuf, size_t klen,
 		   bool tbl_creat, bool excl_creat, uint32_t *table_id,
 		   enum chunk_errcode *err_code);
-extern int fs_obj_do_sum(const char *fn, unsigned int klen, char **csump);
+extern int fs_obj_do_sum(const char *fn, unsigned int klen, unsigned char *md);
 
 /* object.c */
 extern bool object_del(struct client *cli);

chunkd/object.c
@@ -86,7 +86,6 @@ void cli_out_end(struct client *cli)
 static bool object_put_end(struct client *cli)
 {
 	unsigned char md[SHA_DIGEST_LENGTH];
-	char hashstr[50];
 	int rc;
 	enum chunk_errcode err = che_InternalError;
 	bool rcb;
@@ -103,15 +102,13 @@ static bool object_put_end(struct client *cli)
 	cli->state = evt_recycle;
 
 	SHA1_Final(md, &cli->out_hash);
-	hexstr(md, SHA_DIGEST_LENGTH, hashstr);
 
 	rcb = fs_obj_write_commit(cli->out_bo, cli->out_user,
-				  hashstr, (cli->creq.flags & CHF_SYNC));
+				  md, (cli->creq.flags & CHF_SYNC));
 	if (!rcb)
 		goto err_out;
 
-	memcpy(resp->checksum, hashstr, sizeof(hashstr));
-	resp->checksum[sizeof(hashstr)] = 0;
+	memcpy(resp->hash, md, sizeof(resp->hash));
 
 	cli_out_end(cli);
 
@@ -326,8 +323,7 @@ bool object_get(struct client *cli, bool want_body)
 	cli->in_len = obj->size;
 
 	get_resp->resp.data_len = cpu_to_le64(obj->size);
-	memcpy(get_resp->resp.checksum, obj->hashstr, sizeof(obj->hashstr));
-	get_resp->resp.checksum[sizeof(obj->hashstr)] = 0;
+	memcpy(get_resp->resp.hash, obj->hash, sizeof(obj->hash));
 	get_resp->mtime = cpu_to_le64(obj->mtime);
 
 	rc = cli_writeq(cli, get_resp, sizeof(*get_resp), cli_cb_free, get_resp);
@@ -365,7 +361,6 @@ static void worker_cp_thr(struct worker_info *wi)
 	struct backend_obj *obj = NULL, *out_obj = NULL;
 	enum chunk_errcode err = che_InternalError;
 	unsigned char md[SHA_DIGEST_LENGTH];
-	char hashstr[50];
 
 	buf = malloc(bufsz);
 	if (!buf)
@@ -412,9 +407,8 @@ static void worker_cp_thr(struct worker_info *wi)
 	}
 
 	SHA1_Final(md, &cli->out_hash);
-	hexstr(md, SHA_DIGEST_LENGTH, hashstr);
 
-	if (!fs_obj_write_commit(out_obj, cli->user, hashstr, false))
+	if (!fs_obj_write_commit(out_obj, cli->user, md, false))
 		goto err_out;
 
 	err = che_Success;

chunkd/selfcheck.c
@@ -28,9 +28,9 @@ static void chk_list_objs(struct chk_tls *tls, uint32_t table_id)
 {
 	struct fs_obj_lister lister;
 	char *fn;
-	char *csum, *csum_act;
 	char *owner;
 	unsigned long long size;
+	unsigned char md[CHD_CSUM_SZ], md_act[CHD_CSUM_SZ];
 	time_t mtime;
 	void *key_in;
 	size_t klen_in;
@@ -47,7 +47,7 @@ static void chk_list_objs(struct chk_tls *tls, uint32_t table_id)
 
 	while (fs_list_objs_next(&lister, &fn) > 0) {
 
-		rc = fs_obj_hdr_read(fn, &owner, &csum, &key_in, &klen_in,
+		rc = fs_obj_hdr_read(fn, &owner, md, &key_in, &klen_in,
 				     &size, &mtime);
 		if (rc < 0) {
 			free(fn);
@@ -59,22 +59,27 @@ static void chk_list_objs(struct chk_tls *tls, uint32_t table_id)
 			/* This is pretty much impossible unless OOM */
 			applog(LOG_ERR, "chk: objcache_get failed");
 			free(owner);
-			free(csum);
 			free(key_in);
 			free(fn);
 			break;
 		}
 
-		rc = fs_obj_do_sum(fn, klen_in, &csum_act);
+		rc = fs_obj_do_sum(fn, klen_in, md_act);
 		if (rc) {
 			applog(LOG_INFO, "Cannot compute checksum for %s", fn);
 		} else {
 			if (!objcache_test_dirty(&chunkd_srv.actives, cep)) {
-				if (strcmp(csum, csum_act)) {
+				if (memcmp(md, md_act, sizeof(md))) {
+					char hashstr[(CHD_CSUM_SZ*2) + 1];
+					char hashstr_act[(CHD_CSUM_SZ*2) + 1];
+
+					hexstr(md, CHD_CSUM_SZ, hashstr);
+					hexstr(md_act, CHD_CSUM_SZ,hashstr_act);
+
 					applog(LOG_INFO,
 					       "Checksum mismatch for %s: "
 					       "expected %s actual %s",
-					       fn, csum, csum_act);
+					       fn, hashstr, hashstr_act);
 					fs_obj_disable(fn);
 					/*
 					 * FIXME Suicide the whole server if
@@ -87,12 +92,10 @@ static void chk_list_objs(struct chk_tls *tls, uint32_t table_id)
 			} else {
 				tls->stat_conflict++;
 			}
-			free(csum_act);
 		}
 
 		objcache_put(&chunkd_srv.actives, cep);
 		free(owner);
-		free(csum);
 		free(key_in);
 
 		free(fn);

commit 04825d17303f9dd53267353511ea1c8b6c18843b

Chunk protocol: fix 32/64 wire interoperability
Testing found that tabled and chunkd running on CPUs with different
word length cannot talk to each other.

The bug was introduced by commit ea5d20bc22aeed077312c9c1824e84651af17a16.

The fix is to add named padding that takes the place of the invisible
padding, thus making the layout platform-neutral.

Signed-off-by: Pete Zaitcev <zaitcev@redhat.com>
Signed-off-by: Jeff Garzik <jgarzik@redhat.com>

include/chunk_msg.h
@@ -91,6 +91,7 @@ struct chunksrv_resp {
 	uint32_t		nonce;	/* txn id, copied from request */
 	uint64_t		data_len;		/* len of addn'l data */
 	unsigned char		hash[CHD_CSUM_SZ];	/* SHA1 checksum */
+	unsigned char		rsv2[4];		/* pad for 64 bits */
 };
 
 struct chunksrv_resp_get {